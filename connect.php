874678807c505d6808de4bfd8673bf995457812b
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-01-08 23:14:31 +0100

    Connect to database

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$dbname = 'crud';
+$user = 'root';
+$pass = 'root';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
